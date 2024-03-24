02983872333cb63af1bd5b6d2eff37951fe27c42
Shivkumar Sahu <meghasahu29june2001@gmail.com>
2024-03-24 18:34:42 +0530

    handle signin

--- Route/auth_route.js
+++ Route/auth_route.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import {signupHandler} from '../Controllers/auth_contorller.js'
+import {signupHandler,signinHandler} from '../Controllers/auth_contorller.js'
 
 const router=express.Router();
 
 router.post('/signup',signupHandler)
+router.post('/signin',signinHandler)
+
 
 export default router;
