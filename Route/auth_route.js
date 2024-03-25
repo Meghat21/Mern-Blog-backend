a14a36ad02666cfbc5c93d4e6a89331342377f95
Shivkumar Sahu <meghasahu29june2001@gmail.com>
2024-03-25 08:44:06 +0530

    handle google auth for creating new user and logingg in existing user

--- Route/auth_route.js
+++ Route/auth_route.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import {signupHandler,signinHandler} from '../Controllers/auth_contorller.js'
+import {signupHandler,signinHandler,signinHandlerGoogle} from '../Controllers/auth_contorller.js'
 
 const router=express.Router();
 
 router.post('/signup',signupHandler)
 router.post('/signin',signinHandler)
+router.post('/google',signinHandlerGoogle)
+
 
 
 export default router;
